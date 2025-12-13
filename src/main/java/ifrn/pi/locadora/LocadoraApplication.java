be7ed433acc0c923148570923d4de627cc581226
Ark Zin <alef.carvalho@escolar.ifrn.edu.br>
2025-12-12 22:45:20 -0300

    Criando a opção de poder alugar um filme por QR code

--- src/main/java/ifrn/pi/locadora/LocadoraApplication.java
+++ src/main/java/ifrn/pi/locadora/LocadoraApplication.java
@@ -2,7 +2,7 @@ package ifrn.pi.locadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class LocadoraApplication {
